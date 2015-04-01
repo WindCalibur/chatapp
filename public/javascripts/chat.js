bff7043d875094854e02a5239c75b779f9c366f8
EC2 Default User <ec2-user@ip-172-31-26-12.us-west-2.compute.internal>
2015-04-01 06:46:50 +0000

    V0.1

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,13 +1,20 @@
+
 window.onload = function() {
- 
-    var messages = [];
+    
+    messages = [];
     //var socket = io.connect('http://localhost:3000');
     var socket = io.connect('http://ec2-54-186-153-99.us-west-2.compute.amazonaws.com:3000/');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
- 
+    
+    $('#field').keyup( function(event){
+    if(event.keyCode == 13) {
+            $('#send').click();
+        }
+    });
+    
     socket.on('message', function (data) {
         if(data.message) {
             messages.push(data);
